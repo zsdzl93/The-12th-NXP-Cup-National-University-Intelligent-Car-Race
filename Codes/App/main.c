819281761a10b66addb24acac57a4a1f41591e83
zsdzl93 <736452502@qq.com>
2019-06-25 13:56:52 -0400

    update

--- Codes/App/main.c
+++ Codes/App/main.c
@@ -9,16 +9,13 @@
 
    void main(void)
 {
-  init_all();
-  
-    while(1)
-   {
-     Switch();
-     CameraGet(); //Camera capture information //Choose LCD_ST7735S of USE_LCD in App\inc\MK60_conf.h 
-     dataAnalysis() ;
-     DirectionControl();
-     DirectionControlOutput();   
-     
-   }
-
+	init_all();
+	while(1)
+	{
+		Switch();
+		CameraGet(); //Camera capture information //Choose LCD_ST7735S of USE_LCD in App\inc\MK60_conf.h
+		dataAnalysis() ;
+		DirectionControl();
+		DirectionControlOutput();
+	}
 }
